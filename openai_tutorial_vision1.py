62fb7cf3613b5ea7f3a1875872f20fdbeb134429
parksungjoon0805 <jjoon9908@naver.com>
2024-03-12 15:21:39 +0900

    openai_image

--- openai_tutorial_vision1.py
+++ openai_tutorial_vision1.py
@@ -1,23 +1,21 @@
-from openai import OpenAI
 from dotenv import load_dotenv
 import os
-import base64
+from PIL import Image
 import requests
+from openai import OpenAI
+import base64
 
 
-
-
-
-# Function to encode the image
+#로컬에 있는 이미지 사용
+#Function to encode the image
 def encode_image(image_path):
   with open(image_path, "rb") as image_file:
     return base64.b64encode(image_file.read()).decode('utf-8')
-  
 
 load_dotenv()
-openai_api_key = os.getenv("OPEN_API_KEY")
-client = OpenAI(api_key=openai_api_key)
-Model="gpt-4-vision-preview"
+openai_api_key = os.getenv("OPENAI_API_KEY")
+MODEL = "gpt-4-vision-preview"
+
 
 image_path = "image.jpg"
 base64_image = encode_image(image_path)
